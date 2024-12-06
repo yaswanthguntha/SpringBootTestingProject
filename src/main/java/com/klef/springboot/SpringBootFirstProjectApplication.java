00045fe0789d5fb19e527a4f3e64f68aaf13911f
yaswanthguntha <yaswanthguntha7@gmail.com>
2024-12-06 21:33:49 +0530

    V1.0

---
+++ src/main/java/com/klef/springboot/SpringBootFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.klef.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstProjectApplication.class, args);
+		System.out.println("Hi Spring Boot, How are You? We are going to play with you for next one month");
+	}
+
+}
---
+++ src/test/java/com/klef/springboot/SpringBootFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
